eb5981077699b1b16579760cd288d747874dee81
麻凡(dengmengmian) <my@dengmengmian.com>
2023-05-11 16:17:02 +0800

    feat: 增加short_id

--- pkg/util/id/id.go
+++ pkg/util/id/id.go
@@ -1,18 +1,21 @@
+// Copyright 2023 Innkeeper dengmengmian(麻凡) <my@dengmengmian.com>. All rights reserved.
+// Use of this source code is governed by a Apache style
+// license that can be found in the LICENSE file. The original repo for
+// this file is https://github.com/dengmengmian/solocms
+
 package id
 
 import (
 	"strings"
-
-	shortid "github.com/jasonsoft/go-short-id"
 )
 
 // GenShortID 生成 6 位字符长度的唯一 ID.
 func GenShortID() string {
-	opt := shortid.Options{
+	opt := Options{
 		Number:        4,
 		StartWithYear: true,
 		EndWithHost:   false,
 	}
 
-	return strings.ToLower(shortid.Generate(opt))
+	return strings.ToLower(Generate(opt))
 }
